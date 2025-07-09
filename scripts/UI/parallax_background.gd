e25577f05ee6b5a904cac7bf1fe0072fb0736652
Jorge <jorge.contreras.g@ug.uchile.cl>
2025-07-09 11:20:37 -0400

    fix: add loop audio

--- scripts/UI/parallax_background.gd
+++ scripts/UI/parallax_background.gd
@@ -4,13 +4,20 @@ extends ParallaxBackground
 
 var scroll_speed := Vector2(50, 0)
 var loop_length := 6000
+var is_active := true
 
 func _ready() -> void:
 	await get_tree().create_timer(7).timeout
 	intro_audio.play()
 	
 func _process(delta):
+	if !is_active:
+		return
 	scroll_offset.x += scroll_speed.x * delta
 	
 	if scroll_offset.x >= loop_length:
 		scroll_offset.x -= loop_length
+
+
+func _on_loop_finished() -> void:
+	loop_audio.play()
